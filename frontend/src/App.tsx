676844bc3c5ee38a201b4e8cc48d38aa4a8107d7
Julia Camino <caminogjulia@gmail.com>
2023-10-05 18:42:55 -0400

    payment feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Home from "./Home/Home";
 import Products from "./Products/Products";
 import OrderDetail from './Orders/OrderDetail';
 import Orders from './Orders/Orders';
+import Payment from './Payment/Payment';
 
 function App() {
   const openMenu = () => {
@@ -18,7 +19,7 @@ function App() {
     <Router>
     <div className="grid-container">
         <title>Unknown Koala</title>
-        <link rel="stylesheet" href="style.css"/>
+        <link rel="stylesheet" href="index.css"/>
 
         <body>
         <div>
@@ -31,6 +32,7 @@ function App() {
                 <Link to="/">Home</Link>
                 <Link to="/catalog">Catalog</Link>
                 <Link to="/orders">Orders</Link>
+                <Link to="/payment">Payment</Link>
             </div>
         </header>
         <aside className="sidebar">
@@ -54,6 +56,7 @@ function App() {
                 <Route path="/catalog" element={<Products />} />
                 <Route path= "orders" element={<Orders />}></Route>
                 <Route path= "/order/:id" element={<OrderDetail/>}></Route>
+                <Route path="/payment" element={<Payment />}></Route>
             </Routes>
         </main>
         <footer className ="footer">
